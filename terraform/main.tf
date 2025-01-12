d7465f2d3673838bff5e958d9c8476634024ba60
suiwave <184898096+suiwave@users.noreply.github.com>
2025-01-12 10:21:37 +0900

    localでの検証完了。lambdaにあげてみる準備中

---
+++ terraform/main.tf
@@ -0,0 +1,90 @@
+############################################################################
+## terraformブロック
+############################################################################
+terraform {
+  # Terraformのバージョン指定
+  required_version = "~> 1.7.0"
+
+  # Terraformのaws用ライブラリのバージョン指定
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33.0"
+    }
+  }
+}
+
+############################################################################
+## providerブロック
+############################################################################
+provider "aws" {
+  # リージョンを指定
+  region = "ap-northeast-1"
+}
+
+locals {
+  project = "lambda-localstack"
+}
+
+############################################################################
+## resourceブロック
+############################################################################
+resource "aws_iam_role" "lambda_execution_role" {
+    name = "${local.project}-execution-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "lambda.amazonaws.com"
+                }
+            }
+        ]
+    })
+}
+
+resource "aws_iam_policy" "lambda_s3_policy" {
+    name        = "${local.project}-s3-policy"
+    description = "Policy for Lambda to access S3"
+    policy      = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = [
+                    "s3:ListBucket",
+                    "s3:ListBucketVersions",
+                    "s3:ListBucketMultipartUploads"
+                ]
+                Effect   = "Allow"
+                Resource = "*"
+            },
+            {
+                Action = [
+                    "s3:ListObjects",
+                    "s3:ListObjectsV2",
+                    "s3:GetObject"
+                ]
+                Effect   = "Allow"
+                Resource = "*"
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
+    role       = aws_iam_role.lambda_execution_role.name
+    policy_arn = aws_iam_policy.lambda_s3_policy.arn
+}
+
+# resource "aws_lambda_function" "example" {
+#     function_name = "${local.project}-function"
+#     role          = aws_iam_role.lambda_execution_role.arn
+#     handler       = "index.handler"
+#     runtime       = "nodejs18.x"
+#     filename      = "lambda_function_payload.zip"
+
+#     source_code_hash = filebase64sha256("lambda_function_payload.zip")
+# }
